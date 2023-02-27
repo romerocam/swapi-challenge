2353fdf7d61ca2923e8f959ae863599cd79f5ff4
romerocam <romerocam@hotmail.com>
2023-02-27 08:52:25 -0300

    tests

--- components/CharacterList.jsx
+++ components/CharacterList.jsx
@@ -9,7 +9,7 @@ const CharacterList = ({ props }) => {
   console.log("PROPS", props);
   console.log("NEXT", next);
   // console.log("PREVIOUS", previous);
-  // console.log("CHARACTERS", characters);
+  console.log("CHARACTERS", characters);
 
   const handleNextPage = () => {
     axios.get(next).then((response) => {
