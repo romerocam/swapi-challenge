e436e1ee9f851e51ee241c99c3f302a175c84706
Juan Camilo Romero <101602875+romerocam@users.noreply.github.com>
2024-03-11 16:26:36 -0300

    Update CharacterList.jsx

--- components/CharacterList.jsx
+++ components/CharacterList.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import Image from "next/legacy/image";
+import Image from "next/legacy/image" ;
 
 const CharacterList = ({ props }) => {
   const [characters, setCharacters] = useState(props.results);
